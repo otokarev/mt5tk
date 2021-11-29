c52569b6763fb9cd15cf2746c8d004c2edd7b4cc
Oleg Tokarev <otokarev@gmail.com>
2021-11-29 22:14:13 +0700

    * removed odd sybol action

--- interanal/cmd/symbol/symbol.go
+++ interanal/cmd/symbol/symbol.go
@@ -1,7 +1,6 @@
 package symbol
 
 import (
-	"fmt"
 	"github.com/otokarev/mt5tk/pkg/model"
 	"github.com/spf13/cobra"
 )
@@ -13,9 +12,6 @@ func Build(factory *model.Factory) *cobra.Command {
 	var symbolCmd = &cobra.Command{
 		Use:   "symbol",
 		Short: "A brief description of your command",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("symbol called")
-		},
 	}
 
 	symbolCmd.AddCommand(buildGet())
